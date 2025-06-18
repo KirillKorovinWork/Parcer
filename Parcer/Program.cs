6d78c3b01f6bafc85ffd9614bb69c11dbd71cab9
Kirill Korovin <144453127+KirillKorovinWork@users.noreply.github.com>
2025-06-18 21:51:01 +0200

    configure file path

--- Parcer/Program.cs
+++ Parcer/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using ClosedXML.Excel;
+using Newtonsoft.Json;
 
 namespace Parcer
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            var baseDir = @"C:\Users\Korov\Documents\ChillBase";
+            var excelFile = Path.Combine(baseDir, "Tech GD Test Parcer.xlsx");
+            var outputJson = Path.Combine(baseDir, "balances_config.json");
+
+            var workbook = new XLWorkbook(excelFile);
+            var sheet = workbook.Worksheet("Balance");
+
         }
     }
 }
